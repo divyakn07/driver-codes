635257dd831129e716bb8b4d8e282afbd100a68c
K N DIVYA <96383946+divyakn07@users.noreply.github.com>
2022-04-08 13:01:56 +0530

    Update cpp-driver-code.cpp

--- numbers/cpp-driver-code.cpp
+++ numbers/cpp-driver-code.cpp
@@ -3,26 +3,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string solve (int n) {
-   // Your code goes here
-   // n is the given input
-   return "Special";
+bool solve (int n) {
+  int sum=0;
+   while(n>0|| sum>9)
+   {
+      if(n==0)
+      {
+         n=sum;
+         sum=0;
+      }
+      sum+=n%10;
+      n/=10;
+   }
+   return (sum==1);
 }
 
-// The main function reads the input in the required format.
-// Just complete the solve function above.
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int T;
-    cin >> T;
-    for(int t_i = 0; t_i < T; t_i++)
-    {
-        int n;
-        cin >> n;
-        string out_;
-        out_ = solve(n);
-        cout << out_;
-        cout << "\n";
-    }
-}
+      int n=1234;
+      if(solve(n))
+         cout<<"Magic number";
+      else
+         cout<<"Normal number";
+      return 0;
+   }
